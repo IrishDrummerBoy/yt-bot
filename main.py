031c65ea9ec5e2539565b1a2fa88869b37877f93
IrishDrummerBoy <74676119+IrishDrummerBoy@users.noreply.github.com>
2023-01-18 23:28:55 -0600

    Implement download history

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os
+import csv
 from yt_dlp import YoutubeDL
-from yt_dlp.utils import DownloadError
+
+download_history_csv = "download_history.csv"
+
+if not os.path.isfile(download_history_csv):
+	with open(download_history_csv, "w") as csv_file:
+		csv_file.close()
+		pass
+
+	print("File history csv was created successfully!")
 
 class loggerOutputs:
 	def error(msg):
@@ -10,6 +20,26 @@ class loggerOutputs:
 
 	def debug(msg):
 		print(msg)
+
+def get_array_from_csv(csv_path):
+	array = []
+
+	with open(csv_path) as csv_array:
+		reader = csv.reader(csv_array)
+
+		for row in reader:
+			array.append(row)
+		csv_array.close()
+
+	return array
+
+def append_to_csv(data, csv_path):
+	row = [data]
+	with open(csv_path, "a") as csv_object:
+		writer_object = csv.writer(csv_object)
+		writer_object.writerow(row)
+		csv_object.close()
+
 
 def download_mp3(url):
 	options = {
@@ -33,16 +63,18 @@ def download_mp4(url):
 
 def main():
 	try:
+		download_history_array = get_array_from_csv(download_history_csv)
+		print(download_history_array)
+
 		while True:
 			url = input("Please enter a video link or type 'exit' to quit: ")
 
 			if (url != "exit"):
 				download_mp4(url)
+				append_to_csv(url, download_history_csv)
+
 			elif(url == "exit"):
 				break
-
-	except DownloadError:
-		print(DownloadError)
 
 	finally:
 		print("Thanks for using yt-bot!")
